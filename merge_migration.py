8643bf699e98646575f6133ae304f2312ab5295b
abdoconan <abdo1258conan@gmail.com>
2024-06-11 17:14:26 +0200

    fixed error in python migration file

--- merge_migration.py
+++ merge_migration.py
@@ -3,9 +3,7 @@ from src import create_app
 from flask_migrate import current
 
 
-if __name__ == "__main":
-
-    app = create_app()
+if __name__ == "__main__":
     with open('changed_files.txt', 'r') as file:
         changed_files = file.readlines()
 
@@ -13,6 +11,10 @@ if __name__ == "__main":
     for changed_file in changed_files:
         print(changed_file.strip())
 
+    
+    app = create_app()
     with app.app_context():
         current_revision = current(directory="migrations", verbose=True)
+
+    
 

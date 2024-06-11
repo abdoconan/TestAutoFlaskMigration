25600290e6484d4ab0af80a86d70a7c1a4a5b197
abdoconan <abdo1258conan@gmail.com>
2024-06-11 16:52:38 +0200

    Modifeid the script

--- scripts/merge_migration.py
+++ scripts/merge_migration.py
@@ -1,6 +1,16 @@
-with open('changed_files.txt', 'r') as file:
-    changed_files = file.readlines()
 
-# Process the list of changed files
-for changed_file in changed_files:
-    print(changed_file.strip())
+from app import app
+from flask_migrate import current
+
+
+if __name__ == "__main":
+    with open('changed_files.txt', 'r') as file:
+        changed_files = file.readlines()
+
+    # Process the list of changed files
+    for changed_file in changed_files:
+        print(changed_file.strip())
+
+    with app.app_context():
+        print(current())
+

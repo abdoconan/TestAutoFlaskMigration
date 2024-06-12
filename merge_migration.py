393c6a13b999b6adba234f055071540dec7827fb
abdoconan <abdo1258conan@gmail.com>
2024-06-12 08:31:09 +0200

    Modified

--- merge_migration.py
+++ merge_migration.py
@@ -1,20 +1,17 @@
 
 from src import create_app
-from flask_migrate import current
+from flask_migrate import current, heads
 
 
 if __name__ == "__main__":
-    with open('changed_files.txt', 'r') as file:
-        changed_files = file.readlines()
+    # with open('changed_files.txt', 'r') as file:
+    #     changed_files = file.readlines()
 
-    # Process the list of changed files
-    for changed_file in changed_files:
-        print(changed_file.strip())
-
-    
-    app = create_app()
-    with app.app_context():
-        current_revision = current(directory="migrations", verbose=True)
+    # # Process the list of changed files
+    # for changed_file in changed_files:
+    #     print(changed_file.strip())
+    with create_app().app_context():
+        heads(directory="migrations", verbose=False)
 
     
 
